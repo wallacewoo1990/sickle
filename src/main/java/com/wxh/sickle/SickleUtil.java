b2b059a87afc93c102d32d12d8391b5efe29d253
wallacewoo1990 <363671035@qq.com>
2018-06-05 20:15:39 +0800

    v0.0.3

--- src/main/java/com/wxh/sickle/SickleUtil.java
+++ src/main/java/com/wxh/sickle/SickleUtil.java
@@ -3,7 +3,18 @@ package main.java.com.wxh.sickle;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * 镰刀字符串工具类
+ * @author wxh 
+ * @time 2018年6月5日20:13:37
+ *
+ */
 public class SickleUtil {
+	
+	/**
+	 * 将系统输入输出成('','','')的形式
+	 * by wxh 2018年6月5日20:13:12
+	 */
 	public static void convertToInParams(){
 		//scanner对象监控系统输入
 		Scanner scanner = new Scanner(System.in);
